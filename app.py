fe7797350e95f66222d8d96a2c3677e8c91d8733
飯糰 <102470904+ricetw@users.noreply.github.com>
2023-10-30 13:49:46 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import datetime
 import json
 import os
-
-from dotenv import load_dotenv
 import paho.mqtt.client as mqtt
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from models import (
-    BondingMachine1, BondingMachine2, ChipRemovalMachine, DrillerMachine1, DrillerMachine2, RollerMachine, SawingMachine
+    EnergyConsumption
 )
 
-
-tz_delta = datetime.timedelta(hours=8)
+tz_delta = datetime.timedelta(hours=0)
 tz = datetime.timezone(tz_delta)
 current_directory = os.path.dirname(os.path.abspath(__file__))
 dotenv_path = os.path.join(current_directory, "./", ".env")
@@ -23,203 +23,117 @@ Session = sessionmaker(bind=engine)
 
 client = mqtt.Client()
 
+task = BackgroundScheduler(timezone="Asia/Taipei")
+
+global electric_current
+electric_current = {
+    "frequency": 60,
+    "Uan": 110,
+    "Ubn": 110,
+    "Ucn": 110,
+    "lnAV": 110,
+    "Uab": 190.52,
+    "Ubc": 190.52,
+    "Uca": 190.52,
+    "PFAV": 100,
+    "kVarH": 0
+}
+
+def timenow():
+    return datetime.datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+
+
+def add_electric_current(data):
+    print(f"electric_current - {data}")
+    try:
+        with Session.begin() as session:
+            session.add(EnergyConsumption(
+                frequency=data["frequency"],
+                Uan=data["Uan"],
+                Ubn=data["Ubn"],
+                Ucn=data["Ucn"],
+                lnAV=data["lnAV"],
+                Uab=data["Uab"],
+                Ubc=data["Ubc"],
+                Uca=data["Uca"],
+                PFAV=data["PFAV"],
+                kVarH=data["kVarH"],
+                Ia=data["Ia"],
+                Ib=data["Ib"],
+                Ic=data["Ic"],
+                In=data["In"],
+                IAV=data["IAV"],
+                kWa=data["kWa"],
+                kWb=data["kWb"],
+                kWc=data["kWc"],
+                kW=data["kW"],
+                kWH_total=data["kWH_total"],
+                kWH_last1H=data["kWH_last1H"],
+                demand=data["demand"],
+                timestamp=timenow()
+            ))
+            electric_current.clear()
+    except Exception as e:
+        print('Error: ', e)
+            
+
 
 @client.connect_callback()
 def on_connect(clinet, userdata, flags, rc):
     print(f"=============== {'Connect':^15} ===============")
-    client.subscribe("devices/bonding_machine1/#")
-    client.subscribe("devices/bonding_machine2/#")
-    client.subscribe("devices/chip_removal_machine/#")
-    client.subscribe("devices/driller_machine1/#")
-    client.subscribe("devices/driller_machine2/#")
-    client.subscribe("devices/roller_machine/#")
-    client.subscribe("devices/sawing_machine/#")
-    # client.subscribe("devices/#")
+    client.subscribe("devices/ZG_GW02_pwrmeter02/messages/events/")
 
 
 @client.message_callback()
 def on_message(client, userdata, msg):
-    data = json.loads(msg.payload.decode('utf-8'))
-    # print(f"{msg.topic} - {data}")
-    with Session.begin() as session:
-        if msg.topic == "devices/bonding_machine1/messages/events/":
-            session.add(BondingMachine1(
-                status=data["status"],
-                demand=data["demand"],
-                frequency=data["frequency"],
-                Uan=data["Uan"],
-                Ubn=data["Ubn"],
-                Ucn=data["Ucn"],
-                lnAV=data["lnAV"],
-                Uab=data["Uab"],
-                Ubc=data["Ubc"],
-                Uca=data["Uca"],
-                Ia=data["Ia"],
-                Ib=data["Ib"],
-                Ic=data["Ic"],
-                IAV=data["IAV"],
-                kWa=data["kWa"],
-                kWb=data["kWb"],
-                kWc=data["kWc"],
-                kW=data["kW"],
-                kWH=data["kWH"],
-                PFAV=data["PFAV"],
-                kVarH=data["kVarH"],
-                datetime=data["datetime"]
-            ))
-        elif msg.topic == "devices/bonding_machine2/messages/events/":
-            session.add(BondingMachine2(
-                status=data["status"],
-                demand=data["demand"],
-                frequency=data["frequency"],
-                Uan=data["Uan"],
-                Ubn=data["Ubn"],
-                Ucn=data["Ucn"],
-                lnAV=data["lnAV"],
-                Uab=data["Uab"],
-                Ubc=data["Ubc"],
-                Uca=data["Uca"],
-                Ia=data["Ia"],
-                Ib=data["Ib"],
-                Ic=data["Ic"],
-                IAV=data["IAV"],
-                kWa=data["kWa"],
-                kWb=data["kWb"],
-                kWc=data["kWc"],
-                kW=data["kW"],
-                kWH=data["kWH"],
-                PFAV=data["PFAV"],
-                kVarH=data["kVarH"],
-                datetime=data["datetime"]
-            ))
-        elif msg.topic == "devices/chip_removal_machine/messages/events/":
-            session.add(ChipRemovalMachine(
-                status=data["status"],
-                demand=data["demand"],
-                frequency=data["frequency"],
-                Uan=data["Uan"],
-                Ubn=data["Ubn"],
-                Ucn=data["Ucn"],
-                lnAV=data["lnAV"],
-                Uab=data["Uab"],
-                Ubc=data["Ubc"],
-                Uca=data["Uca"],
-                Ia=data["Ia"],
-                Ib=data["Ib"],
-                Ic=data["Ic"],
-                IAV=data["IAV"],
-                kWa=data["kWa"],
-                kWb=data["kWb"],
-                kWc=data["kWc"],
-                kW=data["kW"],
-                kWH=data["kWH"],
-                PFAV=data["PFAV"],
-                kVarH=data["kVarH"],
-                datetime=data["datetime"]
-            ))
-        elif msg.topic == "devices/driller_machine1/messages/events/":
-            session.add(DrillerMachine1(
-                status=data["status"],
-                demand=data["demand"],
-                frequency=data["frequency"],
-                Uan=data["Uan"],
-                Ubn=data["Ubn"],
-                Ucn=data["Ucn"],
-                lnAV=data["lnAV"],
-                Uab=data["Uab"],
-                Ubc=data["Ubc"],
-                Uca=data["Uca"],
-                Ia=data["Ia"],
-                Ib=data["Ib"],
-                Ic=data["Ic"],
-                IAV=data["IAV"],
-                kWa=data["kWa"],
-                kWb=data["kWb"],
-                kWc=data["kWc"],
-                kW=data["kW"],
-                kWH=data["kWH"],
-                PFAV=data["PFAV"],
-                kVarH=data["kVarH"],
-                datetime=data["datetime"]
-            ))
-        elif msg.topic == "devices/driller_machine2/messages/events/":
-            session.add(DrillerMachine2(
-                status=data["status"],
-                demand=data["demand"],
-                frequency=data["frequency"],
-                Uan=data["Uan"],
-                Ubn=data["Ubn"],
-                Ucn=data["Ucn"],
-                lnAV=data["lnAV"],
-                Uab=data["Uab"],
-                Ubc=data["Ubc"],
-                Uca=data["Uca"],
-                Ia=data["Ia"],
-                Ib=data["Ib"],
-                Ic=data["Ic"],
-                IAV=data["IAV"],
-                kWa=data["kWa"],
-                kWb=data["kWb"],
-                kWc=data["kWc"],
-                kW=data["kW"],
-                kWH=data["kWH"],
-                PFAV=data["PFAV"],
-                kVarH=data["kVarH"],
-                datetime=data["datetime"]
-            ))
-        elif msg.topic == "devices/roller_machine/messages/events/":
-            session.add(RollerMachine(
-                status=data["status"],
-                demand=data["demand"],
-                frequency=data["frequency"],
-                Uan=data["Uan"],
-                Ubn=data["Ubn"],
-                Ucn=data["Ucn"],
-                lnAV=data["lnAV"],
-                Uab=data["Uab"],
-                Ubc=data["Ubc"],
-                Uca=data["Uca"],
-                Ia=data["Ia"],
-                Ib=data["Ib"],
-                Ic=data["Ic"],
-                IAV=data["IAV"],
-                kWa=data["kWa"],
-                kWb=data["kWb"],
-                kWc=data["kWc"],
-                kW=data["kW"],
-                kWH=data["kWH"],
-                PFAV=data["PFAV"],
-                kVarH=data["kVarH"],
-                datetime=data["datetime"]
-            ))
-        elif msg.topic == "devices/sawing_machine/messages/events/":
-            session.add(SawingMachine(
-                status=data["status"],
-                demand=data["demand"],
-                frequency=data["frequency"],
-                Uan=data["Uan"],
-                Ubn=data["Ubn"],
-                Ucn=data["Ucn"],
-                lnAV=data["lnAV"],
-                Uab=data["Uab"],
-                Ubc=data["Ubc"],
-                Uca=data["Uca"],
-                Ia=data["Ia"],
-                Ib=data["Ib"],
-                Ic=data["Ic"],
-                IAV=data["IAV"],
-                kWa=data["kWa"],
-                kWb=data["kWb"],
-                kWc=data["kWc"],
-                kW=data["kW"],
-                kWH=data["kWH"],
-                PFAV=data["PFAV"],
-                kVarH=data["kVarH"],
-                datetime=data["datetime"]
-            ))
+    try:
+        data = json.loads(msg.payload.decode('utf-8'))
+    except:
+        data = msg.payload.decode('utf-8')
+    print(f"{msg.topic} - {data}")
+    if msg.topic == "devices/ZG_GW02_pwrmeter02/messages/events/":
+        data = msg.payload.decode('utf-8').split(" | ")
+        for datainfo in data:
+            dataout = {}
+            for info in datainfo.split("  "):
+                dataout[info.split(":")[0]] = info.split(":")[1]
+            if dataout["Name"] == "Ia":
+                electric_current["Ia"] = float(dataout["Value"])
+            elif dataout["Name"] == "Ib":
+                electric_current["Ib"] = float(dataout["Value"])
+            elif dataout["Name"] == "Ic":
+                electric_current["Ic"] = float(dataout["Value"])
+            elif dataout["Name"] == "In":
+                electric_current["In"] = float(dataout["Value"])
+            else:
+                electric_current["IAV"] = float(dataout["Value"])
+        electric_current["kWa"] = float((electric_current["Ia"]*electric_current["Uan"])/1000)
+        electric_current["kWb"] = float((electric_current["Ib"]*electric_current["Uan"])/1000)
+        electric_current["kWc"] = float((electric_current["Ic"]*electric_current["Uan"])/1000)
+        session = Session()
+        dbdata = session.query(EnergyConsumption).order_by(EnergyConsumption.id.desc()).first()
+        if dbdata:
+            electric_current['kW'] = float(dbdata.kW) + electric_current["kWa"] + electric_current["kWb"] + electric_current["kWc"]
+        else:
+            electric_current["kW"] = float(electric_current["kWa"]+electric_current["kWb"]+electric_current["kWc"])
+        if 0 <= datetime.datetime.now().second < 5 and datetime.datetime.now().minute == 0:
+            electric_current["kWH_total"] = electric_current['kW'] /3600
+            electric_current["kWH_last1H"] = electric_current["kWH_total"] - float(dbdata.kWH_total)
+        elif dbdata == None:
+            electric_current["kWH_total"] = 0
+            electric_current["kWH_last1H"] = 0
+            electric_current["demand"] = 0
+        else:
+            electric_current["kWH_total"] = float(dbdata.kWH_total)
+            electric_current["kWH_last1H"] = float(dbdata.kWH_last1H)
+        if electric_current["kWH_last1H"] > float(dbdata.demand):
+            electric_current["demand"] = electric_current["kWH_last1H"]
+        else:
+            electric_current["demand"] = float(dbdata.demand)
 
 
 if __name__ == "__main__":
     client.connect(os.environ.get("MQTT_IP"),
                    int(os.environ.get("MQTT_PORT")))
+    task.add_job(add_electric_current, 'interval', seconds=10, args=[electric_current])
     client.loop_forever()
--- models.py
+++ models.py
@@ -2,7 +2,7 @@ import datetime
 import os
 
 from dotenv import load_dotenv
-from sqlalchemy import Boolean, create_engine, Column, DateTime, Float, Integer, String
+from sqlalchemy import Boolean, create_engine, Column, DateTime, Float, Integer, String, TIMESTAMP
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -12,9 +12,27 @@ class BaseTable():
     __table_args__ = {
         "mysql_charset": "utf8mb4"
     }
-    Count_Log = Column(Integer, nullable=False, primary_key=True)
-    status = Column(Boolean, nullable=False)
-    demand = Column(Float(5, 2), nullable=False)
+    id = Column(Integer, nullable=False, primary_key=True)
+    timestamp = Column(TIMESTAMP, nullable=False,
+                        default=datetime.datetime.utcnow())
+
+
+class BaseTableForEM():
+    __table_args__ = {
+        "mysql_charset": "utf8mb4"
+    }
+    id = Column(Integer, nullable=False, primary_key=True)
+    timestamp = Column(TIMESTAMP, nullable=False,
+                        default=datetime.datetime.utcnow())
+    I1 = Column(Float(5, 2), nullable=False)
+    I2 = Column(Float(5, 2), nullable=False)
+    I3 = Column(Float(5, 2), nullable=False)
+    EffectivePower = Column(Float(5, 2), nullable=False)
+    ReactivePower = Column(Float(5, 2), nullable=False)
+
+
+class EnergyConsumption(BaseTable, Base):
+    __tablename__ = 'EnergyConsumption'
     frequency = Column(Float(5, 2), nullable=False)
     Uan = Column(Float(5, 2), nullable=False)
     Ubn = Column(Float(5, 2), nullable=False)
@@ -23,46 +41,95 @@ class BaseTable():
     Uab = Column(Float(5, 2), nullable=False)
     Ubc = Column(Float(5, 2), nullable=False)
     Uca = Column(Float(5, 2), nullable=False)
+    PFAV = Column(Float(5, 2), nullable=False)
+    kVarH = Column(Float(5, 2), nullable=False)
     Ia = Column(Float(5, 2), nullable=False)
     Ib = Column(Float(5, 2), nullable=False)
     Ic = Column(Float(5, 2), nullable=False)
+    In = Column(Float(5, 2), nullable=False)
     IAV = Column(Float(5, 2), nullable=False)
     kWa = Column(Float(5, 2), nullable=False)
     kWb = Column(Float(5, 2), nullable=False)
     kWc = Column(Float(5, 2), nullable=False)
     kW = Column(Float(5, 2), nullable=False)
-    kWH = Column(Float(5, 2), nullable=False)
-    PFAV = Column(Float(5, 2), nullable=False)
-    kVarH = Column(Float(5, 2), nullable=False)
-    datetime = Column(DateTime(timezone=False), nullable=False)
-    
-
-class BondingMachine1(BaseTable, Base): # 接合機1
-    __tablename__ = 'BondingMachine1'
+    kWH_total = Column(Float(5, 2), nullable=False)
+    kWH_last1H = Column(Float(5, 2), nullable=False)
+    demand = Column(Float(5, 2), nullable=False)
 
 
-class BondingMachine2(BaseTable, Base): # 接合機2
-    __tablename__ = 'BondingMachine2'
+class EM1(BaseTableForEM, Base):
+    __tablename__ = 'EM1'
 
 
-class DrillerMachine1(BaseTable, Base): # 鑽孔機1
-    __tablename__ = 'DrillerMachine1'
+class EM2(BaseTableForEM, Base):
+    __tablename__ = 'EM2'
 
 
-class DrillerMachine2(BaseTable, Base): # 鑽孔機2
-    __tablename__ = 'DrillerMachine2'
+class EM3(BaseTableForEM, Base):
+    __tablename__ = 'EM3'
 
 
-class RollerMachine(BaseTable, Base): # 捲圓機
-    __tablename__ = 'RollerMachine'
+class EM4(BaseTableForEM, Base):
+    __tablename__ = 'EM4'
 
 
-class ChipRemovalMachine(BaseTable, Base): # 排屑機
-    __tablename__ = 'ChipRemovalMachine'
+class EM5(BaseTableForEM, Base):
+    __tablename__ = 'EM5'
 
 
-class SawingMachine(BaseTable, Base): # 鋸切機
-    __tablename__ = 'SawingMachine'
+class EM6(BaseTableForEM, Base):
+    __tablename__ = 'EM6'
+
+
+class EM7(BaseTableForEM, Base):
+    __tablename__ = 'EM7'
+
+
+class EM8(BaseTableForEM, Base):
+    __tablename__ = 'EM8'
+
+
+class EM9(BaseTableForEM, Base):
+    __tablename__ = 'EM9'
+
+
+class EM10(BaseTableForEM, Base):
+    __tablename__ = 'EM10'
+
+
+class EM11(BaseTableForEM, Base):
+    __tablename__ = 'EM11'
+
+
+class EM12(BaseTableForEM, Base):
+    __tablename__ = 'EM12'
+
+
+class EM14(BaseTable, Base):
+    __tablename__ = 'EM14'
+    flow_rate = Column(Float(5, 2), nullable=False)
+    flow = Column(Float(5, 2), nullable=False)
+
+class EM15(BaseTable, Base):
+    __tablename__ = 'EM15'
+    EM1500 = Column(Float(5, 2), nullable=False)
+    EM1502 = Column(Float(5, 2), nullable=False)
+    EM1504 = Column(Float(5, 2), nullable=False)
+    EM1510 = Column(Float(5, 2), nullable=False)
+    EM1511 = Column(Float(5, 2), nullable=False)
+    EM1512 = Column(Float(5, 2), nullable=False)
+    EM1513 = Column(Float(5, 2), nullable=False)
+    EM1514 = Column(Float(5, 2), nullable=False)
+    EM1515 = Column(Float(5, 2), nullable=False)
+    EM1516 = Column(Float(5, 2), nullable=False)
+    EM1517 = Column(Float(5, 2), nullable=False)
+    EM1518 = Column(Float(5, 2), nullable=False)
+    EM1519 = Column(Float(5, 2), nullable=False)
+    EM1520 = Column(Float(5, 2), nullable=False)
+    EM1521 = Column(Float(5, 2), nullable=False)
+    EM1522 = Column(Float(5, 2), nullable=False)
+    EM1523 = Column(Float(5, 2), nullable=False)
+    EM1524 = Column(Float(5, 2), nullable=False)
 
 
 if __name__ == '__main__':
@@ -73,4 +140,3 @@ if __name__ == '__main__':
     engine = create_engine(os.environ.get("SQL_SERVER"), echo=True)
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
-
